699a6a53b87b292a2162f8b3f7b78ecb52f9d9b0
mohammad hossein <mohammad.aref2011@yahoo.com>
2022-07-23 10:02:19 +0430

    CORE6

---
+++ Core6_Kendo_newTeamplate/Core6_Kendo_newTeamplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Core6_Kendo_newTeamplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
